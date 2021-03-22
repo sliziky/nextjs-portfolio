75513d9b8beee8279fa706b3ca324fdc93ac1091
Peter Bolfa <xbolfa@fi.muni.cz>
2021-03-22 22:04:55 +0100

    init

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import Head  from "next/head"
+import { Component } from 'react'
+import { attributes, react as HomeContent } from '../content/home.md';
+import * as React from 'react';
+export const Home : React.FC = () => {
+  let { title, cats } = attributes;
+  return (
+    <>
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <article>
+        <h1>{title}</h1>
+        <HomeContent />
+        <ul>
+          {cats.map((cat, k) => (
+            <li key={k}>
+              <h2>{cat.name}</h2>
+              <p>{cat.description}</p>
+            </li>
+          ))}
+        </ul>
+      </article>
+    </>
+  )
+}
+export default Home;
